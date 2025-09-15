068659f5bea93347c061258f0fdfacd485ad9f8c
Igor Grosu <tiktokaye2011@gmail.com>
2025-09-15 13:55:44 +0300

    Update clientPanel.tsx

--- src/components/panel/clientPanel.tsx
+++ src/components/panel/clientPanel.tsx
@@ -5,7 +5,7 @@ import { Stack, StackProps } from "@mui/material"
 export function ClientPanel({children, ...props}: StackProps) {
   return <Stack {...props} sx={{
     flex: 1,
-    bgcolor: 'background.default',
+    bgcolor: 'rgba(255, 255, 255, 0.75)',
     p: props.p ?? 2,
     borderRadius: 2,
     ...props.sx
